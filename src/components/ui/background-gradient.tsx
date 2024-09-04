bb3ff4109b3ccf95627c7c579c11d9bd5398fd00
shekher <shekhercse@gmail.com>
2024-09-04 22:06:43 +0530

    done

--- src/components/ui/background-gradient.tsx
+++ src/components/ui/background-gradient.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { utils } from "@/lib/utils";
 import React from "react";
 import { motion } from "framer-motion";
 
@@ -22,7 +22,7 @@ export const BackgroundGradient = ({
         },
     };
     return (
-        <div className={cn("relative p-[4px] group", containerClassName)}>
+        <div className={utils("relative p-[4px] group", containerClassName)}>
             <motion.div
                 variants={animate ? variants : undefined}
                 initial={animate ? "initial" : undefined}
@@ -39,7 +39,7 @@ export const BackgroundGradient = ({
                 style={{
                     backgroundSize: animate ? "400% 400%" : undefined,
                 }}
-                className={cn(
+                className={utils(
                     "absolute inset-0 rounded-3xl z-[1] opacity-60 group-hover:opacity-100 blur-xl  transition duration-500 will-change-transform",
                     " bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
                 )}
@@ -60,13 +60,13 @@ export const BackgroundGradient = ({
                 style={{
                     backgroundSize: animate ? "400% 400%" : undefined,
                 }}
-                className={cn(
+                className={utils(
                     "absolute inset-0 rounded-3xl z-[1] will-change-transform",
                     "bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
                 )}
             />
 
-            <div className={cn("relative z-10", className)}>{children}</div>
+            <div className={utils("relative z-10", className)}>{children}</div>
         </div>
     );
 };
