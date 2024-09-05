84f559a448fb7728997a809571050c7fce32895e
Chander Shekher <shekhercse@gmail.com>
2024-09-05 13:38:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import WhyChooseUs from"@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen bg-gray-900/[0.96] antialiased bg-grid-white/[0.02]">
       
       <HeroSection />
       <Services/>
